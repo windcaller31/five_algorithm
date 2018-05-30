88b9f2ec2a7be9c127d4c2b9e042e538390a3c5a
limengtong <limengtong@iyunxiao.com>
2018-05-30 19:57:03 +0800

    split steal

---
+++ dynamic_programming/better-split-steal.js
@@ -0,0 +1,28 @@
+var q;
+function Memorized_cut_rod(p, n){
+    var r = [];
+    for(var i = 0;i<=n;i++){
+        r[i] = -1000000;
+    }
+    return Memorized_cut_rod_aux(p,n,r);
+}
+
+//最初的CUT_Rod引入备忘机制
+function Memorized_cut_rod_aux(p,n,r){
+    if(r[n]>=0){
+      return r[n];//所需值已知
+    }
+    if(n==0){
+      q = 0;
+    }else{
+      q = -10000000;
+      for(var i = 1;i<=n;i++)
+      q = Math.max(q, p[i]+Memorized_cut_rod_aux(p,n-i,r));
+    }
+    r[n] = q;
+    return q;
+}
+
+var pn = [1,5,8,9,10,17,17,20,24,30];
+var o = 9;
+console.log(Memorized_cut_rod(pn,o));
---
+++ dynamic_programming/split-steal.js
@@ -0,0 +1,22 @@
+/*
+  rn = max{pn, r1+rn-1,r2+rn-2,…..rn-1+r1}
+  pn表示不切割的时候，其他表示切成两段，分别长i 和n-i，接着求解这两段的最优切割收益
+  rn = max(pi + rn-i)
+*/
+
+//价格表p 和长度n
+function cut_rod(p,n){
+    if(n==0){
+      return 0;
+    }
+    var q = -1;
+    for(var i = 1;i<=n;i++){
+      q = Math.max(q, p[i]+ cut_rod(p,n-i));
+    }
+    return q;
+}
+
+var pn = [1,5,8,9,10,17,17,20,24,30];
+var o = 9;
+
+console.log(cut_rod(pn,o));
